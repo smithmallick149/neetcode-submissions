50b53561332ba201ac0475f7a92a9a0c67431764
smith mallick <smith.mallick@yara.com>
2026-05-09 20:24:51 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        while(s.includes('()') || s.includes('{}') || s.includes('[]')){
+            s = s.replace('()', '');
+            s = s.replace('{}', '');
+            s = s.replace('[]','');
+        }
+        return s === '';
+    }
+}
