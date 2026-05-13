7ca894e61412ccf3fa7103bd958b85ec65e8b3c2
smith mallick <smith.mallick@yara.com>
2026-05-13 10:42:09 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-5

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-5.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {TreeNode}
+     */
+//     lowestCommonAncestor(root, p, q) {
+//        if(!root) return null
+//        if(p.val > root.val && q.val > root.val){
+//             return this.lowestCommonAncestor(root.right, p, q);
+//        } else if(p.val < root.val && q.val < root.val) {
+//             return this.lowestCommonAncestor(root.left, p, q);
+//        } else {
+//             return root
+//        }
+//     }
+
+lowestCommonAncestor(root, p, q){
+     let curr = root;
+     while(curr) {
+          if(p.val > curr.val && q.val > curr.val){
+               curr = curr.right
+          } else if(p.val < curr.val && q.val < curr.val) {
+               curr = curr.left
+          } else {
+               return curr
+          }
+     }
+     return null
+}
+}
