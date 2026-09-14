fb000a05a4b97ca16a7196c4a6000b3bc33b6670
smith mallick <smith.mallick@yara.com>
2026-09-14 17:56:55 +0530

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int [] result = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i <nums.length;i++) {            
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+        for(int j=0;j <k;j++) {
+            int maxNum = 0;
+            int maxFreq = 0;
+            for(int num: map.keySet()) {
+                if(map.get(num) > maxFreq) {
+                    maxFreq = map.get(num);
+                    maxNum = num;
+                }
+            }
+            result[j] = maxNum;
+            map.remove(maxNum);
+        }
+        return result;
+    }
+}
