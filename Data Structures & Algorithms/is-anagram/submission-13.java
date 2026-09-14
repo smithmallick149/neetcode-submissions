f06dfd4d8c50d34be85f422620f70ba18af27687
smith mallick <smith.mallick@yara.com>
2026-09-14 16:48:49 +0530

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+
+        for(char c: s.toCharArray()) {
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        }
+        for(char c: t.toCharArray()) {
+            map2.put(c, map2.getOrDefault(c, 0) + 1);
+        }
+        
+    return map1.equals(map2);
+    }
+}
